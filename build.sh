50f5d43b971baa2c91ca53bc7c0f48597cd5feeb
J4BB3R <johanmaurel@protonmail.com>
2018-05-18 01:34:42 +0200

    [Build] Docker v1.0

--- build.sh
+++ build.sh
@@ -5,7 +5,4 @@
 command -v node >/dev/null 2>&1 || { echo >&2 "Install NodeJs v6.0.0 or later"; exit 1; }
 cd client
 npm install
-npm run dist
-cp -R dist/assets ../
-cp dist/index.html ../
-cp src/qimessaging.js ../assets/
+npm run perso
